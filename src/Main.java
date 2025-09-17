9ad83b6f20e622aa1cbce7f3b3805e319a9f996c
shubhamkush78 <shubhamkush780306@gmail.com>
2025-09-17 23:39:00 +0530

    MAD LIBS GAMES

--- src/Main.java
+++ src/Main.java
@@ -4,23 +4,31 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Calculate area of a rectangle
+      // MAD LIBS GAMES
 
-        double width = 0;
-        double height = 0;
-        double area = 0;
+        Scanner scanner = new Scanner (System.in);
 
-        Scanner scanner = new Scanner(System.in);
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
 
-        System.out.println("Enter the width: ");
-        width = scanner.nextDouble();
+        System.out.println("Enter an adjective (Description) : ");
+        adjective1  = scanner.nextLine();
+        System.out.println(" Enter a noun (animal or person) : ");
+        noun1 = scanner.nextLine();
+        System.out.println(" Enter a adjective (Description) : ");
+        adjective2 = scanner.nextLine();
+        System.out.println(" Enter a verb end with -ing (action) : ");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter an adjective (description) : ");
+        adjective3 = scanner.nextLine();
 
-        System.out.println("Enter the height: ");
-        height = scanner.nextDouble();
-
-        area = width * height;
-
-        System.out.println("The area is : " + area + "cm2");
+        System.out.println("Today I went a " + adjective1 + " zoo. " );
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!" );
+        System.out.println("I was " + adjective3 + "!");
 
         scanner.close();
     }
