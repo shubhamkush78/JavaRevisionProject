26134a71f0c3c78b1084369742715125105319da
shubhamkush78 <shubhamkush780306@gmail.com>
2025-09-17 23:09:59 +0530

    Calculate area of a rectangle

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main (String[] args){
+    public static void main(String[] args) {
+
+        // Calculate area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        System.out.println("Enter the width: ");
+        width = scanner.nextDouble();
 
-        System.out.println("Enter your age: ");
-        int age = scanner.nextInt();
+        System.out.println("Enter the height: ");
+        height = scanner.nextDouble();
 
-        System.out.println("Hello " + name);
-        System.out.println("You are " + age + " years old");
+        area = width * height;
+
+        System.out.println("The area is : " + area + "cm2");
 
         scanner.close();
     }
